32ab647c397d078f7f0999fe0943b99a4bcbd641
Jose Blanco <90761900+laicuBlanco@users.noreply.github.com>
2022-04-25 19:26:44 +0100

    refactor as per G comments in PR-1

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -4,21 +4,18 @@ require 'rails_helper'
 
 RSpec.describe "Index page", type: :feature do
 
-
     it "includes the Smartflix header" do
         visit "/"
         expect(page).to have_text("Smartflix")
     end
 
    context "when titles are given" do
-
     it "includes the shows titles" do 
       visit "/" 
       
       csv_data.first(5).each do |show|
         expect(page).to have_text(show['title'])
       end
-      
     end
    end
   
